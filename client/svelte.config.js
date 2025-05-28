87bec6f6f23bb8d32dbe627fd1d2352d82b9bde4
kota-yata <kota@yatagai.com>
2025-05-28 15:31:56 +0900

    update to Svelte5

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -1,12 +1,8 @@
 /// <reference types="vitest" />
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
-  // for more information about preprocessors
-  preprocess: vitePreprocess(),
 
   kit: {
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
