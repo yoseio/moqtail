d65feaa7718564bcb4a6ead76c7ede2455baf068
Kota <kota@yatagai.com>
2025-06-11 23:51:32 +0900

    Remove test tsconfig and adjust Jest configs

--- moqtail-core/jest.config.js
+++ moqtail-core/jest.config.js
@@ -1,11 +1,24 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
+import fs from 'fs';
+import ts from 'typescript';
+
+const rawConfig = ts.readConfigFile(
+  new URL('./tsconfig.json', import.meta.url).pathname,
+  (p) => fs.readFileSync(p, 'utf8')
+).config;
+const tsconfig = {
+  ...rawConfig.compilerOptions,
+  paths: { ...(rawConfig.compilerOptions?.paths || {}), bytes: ['../bytes/src/index.ts'] }
+};
+
 export default {
-  testEnvironment: "node",
+  testEnvironment: 'node',
   transform: {
-    "^.+\\.tsx?$": ["ts-jest", {"tsconfig":"tsconfig.test.json"}],
+    '^.+\\.tsx?$': ['ts-jest', { useESM: true, tsconfig }],
   },
+  extensionsToTreatAsEsm: ['.ts'],
   moduleNameMapper: {
-    "^bytes$": "<rootDir>/../bytes/src/index.ts"
+    '^bytes$': '<rootDir>/../bytes/src/index.ts'
   },
-  testPathIgnorePatterns: ["/node_modules/", "/dist/"]
+  testPathIgnorePatterns: ['/node_modules/', '/dist/']
 };
