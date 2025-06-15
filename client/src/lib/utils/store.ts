53426728d350289487d1e6305cb51565146ec19b
kota-yata <kota@yatagai.com>
2025-06-16 01:07:09 +0900

    add missing default value to RIngBufferStats store

--- client/src/lib/utils/store.ts
+++ client/src/lib/utils/store.ts
@@ -5,7 +5,10 @@ export const moqVideoTransmissionLatencyStore = writable<number>(0);
 export const ringStats = writable<RingBufferStats>({
   capacity: 48000,
   writePos: 0,
-  readPos: 0
+  readPos: 0,
+  size: 0,
+  available: 0,
+  free: 48000,
 });
 
 export const bitrateStore = writable<number>(0);
