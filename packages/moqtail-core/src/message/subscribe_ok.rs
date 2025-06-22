b0821fd79f17417d4bbc835b1f5e41034212c90f
Kousuke Takaki <98276492+yoseio@users.noreply.github.com>
2025-06-22 13:24:20 +0900

    Implement SubscribeOk message

--- packages/moqtail-core/src/message/subscribe_ok.rs
+++ packages/moqtail-core/src/message/subscribe_ok.rs
@@ -1,15 +1,117 @@
-use crate::coding::{Decode, Encode};
+use crate::coding::{Decode, Encode, VarInt};
+use bytes::{Buf, BufMut};
+use crate::model::Parameter;
 
-pub struct SubscribeOk {}
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct SubscribeOk {
+    pub subscribe_id: VarInt,
+    pub expires: VarInt,
+    pub group_order: u8,
+    pub content_exists: u8,
+    pub largest_group_id: Option<VarInt>,
+    pub largest_object_id: Option<VarInt>,
+    pub parameters: Vec<Parameter>,
+}
 
 impl Encode for SubscribeOk {
-    fn encode<B: bytes::BufMut>(&self, _buf: &mut B) {
-        todo!()
+    fn encode<B: BufMut>(&self, buf: &mut B) {
+        self.subscribe_id.encode(buf);
+        self.expires.encode(buf);
+        buf.put_u8(self.group_order);
+        buf.put_u8(self.content_exists);
+        if self.content_exists == 1 {
+            if let Some(gid) = &self.largest_group_id {
+                gid.encode(buf);
+            }
+            if let Some(oid) = &self.largest_object_id {
+                oid.encode(buf);
+            }
+        }
+        VarInt(self.parameters.len() as u64).encode(buf);
+        for p in &self.parameters {
+            p.encode(buf);
+        }
     }
 }
 
 impl<'a> Decode<'a> for SubscribeOk {
-    fn decode<B: bytes::Buf>(_buf: &mut B) -> Result<Self, crate::coding::Error> {
-        todo!()
+    fn decode<B: Buf>(buf: &mut B) -> Result<Self, crate::coding::Error> {
+        let subscribe_id = VarInt::decode(buf)?;
+        let expires = VarInt::decode(buf)?;
+        if !buf.has_remaining() {
+            return Err(crate::coding::Error::UnexpectedEnd);
+        }
+        let group_order = buf.get_u8();
+        if !buf.has_remaining() {
+            return Err(crate::coding::Error::UnexpectedEnd);
+        }
+        let content_exists = buf.get_u8();
+
+        let (largest_group_id, largest_object_id) = if content_exists == 1 {
+            let gid = VarInt::decode(buf)?;
+            let oid = VarInt::decode(buf)?;
+            (Some(gid), Some(oid))
+        } else {
+            (None, None)
+        };
+
+        let num_params = VarInt::decode(buf)?.into_inner() as usize;
+        let mut parameters = Vec::new();
+        for _ in 0..num_params {
+            parameters.push(Parameter::decode(buf)?);
+        }
+
+        Ok(SubscribeOk {
+            subscribe_id,
+            expires,
+            group_order,
+            content_exists,
+            largest_group_id,
+            largest_object_id,
+            parameters,
+        })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn encode_decode_roundtrip_no_content() {
+        let msg = SubscribeOk {
+            subscribe_id: VarInt(1),
+            expires: VarInt(0),
+            group_order: 1,
+            content_exists: 0,
+            largest_group_id: None,
+            largest_object_id: None,
+            parameters: Vec::new(),
+        };
+        let mut buf = bytes::BytesMut::new();
+        msg.encode(&mut buf);
+
+        let mut bytes = buf.freeze();
+        let decoded = SubscribeOk::decode(&mut bytes).unwrap();
+        assert_eq!(decoded, msg);
+    }
+
+    #[test]
+    fn encode_decode_roundtrip_with_content() {
+        let msg = SubscribeOk {
+            subscribe_id: VarInt(2),
+            expires: VarInt(10),
+            group_order: 2,
+            content_exists: 1,
+            largest_group_id: Some(VarInt(5)),
+            largest_object_id: Some(VarInt(7)),
+            parameters: vec![Parameter::DeliveryTimeout(VarInt(3))],
+        };
+        let mut buf = bytes::BytesMut::new();
+        msg.encode(&mut buf);
+
+        let mut bytes = buf.freeze();
+        let decoded = SubscribeOk::decode(&mut bytes).unwrap();
+        assert_eq!(decoded, msg);
     }
 }
--- packages/moqtail-core/src/model.rs
+++ packages/moqtail-core/src/model.rs
@@ -88,3 +88,79 @@ impl<'a> crate::coding::Decode<'a> for SetupParameter {
         }
     }
 }
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum Parameter {
+    AuthorizationInfo(String),
+    DeliveryTimeout(VarInt),
+    MaxCacheDuration(VarInt),
+    Unknown { ty: VarInt, value: bytes::Bytes },
+}
+
+impl crate::coding::Encode for Parameter {
+    fn encode<B: bytes::BufMut>(&self, buf: &mut B) {
+        match self {
+            Parameter::AuthorizationInfo(info) => {
+                VarInt(0x02).encode(buf);
+                VarInt(info.as_bytes().len() as u64).encode(buf);
+                buf.put_slice(info.as_bytes());
+            }
+            Parameter::DeliveryTimeout(v) => {
+                VarInt(0x03).encode(buf);
+                let mut tmp = bytes::BytesMut::new();
+                v.encode(&mut tmp);
+                VarInt(tmp.len() as u64).encode(buf);
+                buf.put_slice(&tmp);
+            }
+            Parameter::MaxCacheDuration(v) => {
+                VarInt(0x04).encode(buf);
+                let mut tmp = bytes::BytesMut::new();
+                v.encode(&mut tmp);
+                VarInt(tmp.len() as u64).encode(buf);
+                buf.put_slice(&tmp);
+            }
+            Parameter::Unknown { ty, value } => {
+                ty.encode(buf);
+                VarInt(value.len() as u64).encode(buf);
+                buf.put_slice(value);
+            }
+        }
+    }
+}
+
+impl<'a> crate::coding::Decode<'a> for Parameter {
+    fn decode<B: bytes::Buf>(buf: &mut B) -> Result<Self, crate::coding::Error> {
+        let ty = VarInt::decode(buf)?;
+        match ty.into_inner() {
+            0x02 => {
+                let len = VarInt::decode(buf)?.into_inner() as usize;
+                if buf.remaining() < len {
+                    return Err(crate::coding::Error::UnexpectedEnd);
+                }
+                let bytes = buf.copy_to_bytes(len);
+                let info = std::str::from_utf8(&bytes)
+                    .map_err(|_| crate::coding::Error::UnexpectedEnd)?
+                    .to_string();
+                Ok(Parameter::AuthorizationInfo(info))
+            }
+            0x03 => {
+                let _len = VarInt::decode(buf)?;
+                let val = VarInt::decode(buf)?;
+                Ok(Parameter::DeliveryTimeout(val))
+            }
+            0x04 => {
+                let _len = VarInt::decode(buf)?;
+                let val = VarInt::decode(buf)?;
+                Ok(Parameter::MaxCacheDuration(val))
+            }
+            _ => {
+                let len = VarInt::decode(buf)?.into_inner() as usize;
+                if buf.remaining() < len {
+                    return Err(crate::coding::Error::UnexpectedEnd);
+                }
+                let bytes = buf.copy_to_bytes(len);
+                Ok(Parameter::Unknown { ty, value: bytes })
+            }
+        }
+    }
+}
